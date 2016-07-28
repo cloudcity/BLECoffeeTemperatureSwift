3780ae8b841da78dba5da889c9bb91845fbfa01c
Evan Stone <evan@cloudcity.io>
2016-07-28 12:09:30 -0700

    Initial Add

---
+++ BLECoffeeTemperatureSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSRemoteConfBLEDemo
+//
+//  Created by Evan Stone on 4/9/16.
+//  Copyright © 2016 Cloud City. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
